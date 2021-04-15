9dabb75fa2c11e1d4dcf4be49f2438fbba32df23
saulzh <z@ruiruizhang.com>
2021-04-15 09:01:03 +0800

    update

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route, Link, } from "react-router-dom";
+import { HashRouter as Router, Switch, Route, Link, } from "react-router-dom";
 import logo from '../logo.svg';
 import Cnode from '../pages/cnode';
 import '../style/header.css';
--- src/pages/cnode.js
+++ src/pages/cnode.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch, Link, Redirect, useParams } from "react-router-dom";
+import { HashRouter as Router, Route, Switch, Link, } from "react-router-dom";
 import '../style/cnode.css';
 import axios from "axios";
 
